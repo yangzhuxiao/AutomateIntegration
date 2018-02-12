f8c30dd5607819bbcd796b9b743b1c70c2841763
yxz <yangzhuxiao@126.com>
2018-02-12 16:46:14 +0800

    added a log

--- TestProject/AppDelegate.m
+++ TestProject/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"Add something");
     return YES;
 }
 
